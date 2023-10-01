bdcb6344c9e2f98e7832c736f52537646268bd73
keshava <katakamkeshava1289@gmail.com>
2023-10-01 19:11:07 +0530

    modified

--- src/main/java/com/example/entity/UserBO.java
+++ src/main/java/com/example/entity/UserBO.java
@@ -1,15 +1,60 @@
 package com.example.entity;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import lombok.*;
 
 @Entity
-@Setter
 @Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@Table(name = "user_details")
+@Table(
+        name = "user_details",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "employee_code"),
+                @UniqueConstraint(columnNames = "email")
+        })
 public class UserBO {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_details_id_generator")
+    @SequenceGenerator(
+            name = "user_details_id_generator",
+            sequenceName = "user_details_id_seq",
+            allocationSize = 1)
+    private Integer id;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @ManyToMany(
+            fetch = FetchType.EAGER,
+            cascade = {CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinTable(
+            name = "user_role_mapping",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<RoleBO> roles = new ArrayList<>();
+
+    @Column(name = "created_on")
+    private Date createdOn;
+
+    @Column(name = "modified_on")
+    private Date modifiedOn;
 }
+
